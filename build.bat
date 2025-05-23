4e0ba9b159fbad18c8e3a9338eb94e9e5c71b0cf
Khanh <ngokhanh809@gmail.com>
2025-05-22 20:14:28 -0700

    Set stack size to 16 mb

--- build.bat
+++ build.bat
@@ -8,11 +8,13 @@ if "clean"=="%1" (
 
     if not exist bin\ mkdir bin
     pushd bin
-        cl /source-charset:utf-8 /Zi -I..\Include ..\Build.c -o Graph.exe ^
-            gdi32.lib user32.lib^
-            /link /subsystem:windows
+        cl /source-charset:utf-8 /Zi -I..\Include ^
+            ..\Build.c /FeGraph.exe gdi32.lib user32.lib^
+            /link /subsystem:windows /stack:0x1000000
     popd
 ) else (
     if not exist bin\ mkdir bin
-    gcc -ggdb -Wall -Wextra -Wpedantic -IInclude Build.c -o bin\Graph.exe -lgdi32 -luser32 -lkernel32
+    gcc -O2 -ggdb -Wall -Wextra -Wpedantic -IInclude ^
+        Build.c -o bin\Graph.exe ^
+        -lgdi32 -luser32 -lkernel32 -Wl,--stack,0x1000000
 )
