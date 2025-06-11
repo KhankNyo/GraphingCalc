0f918e6c8dc31200d708886c93fe70f70f0ac0a5
Khanh <ngokhanh809@gmail.com>
2025-06-10 22:27:03 -0700

    Added support for calling build.bat with different compilers

--- build.bat
+++ build.bat
@@ -1,22 +1,33 @@
 @echo off
+SETLOCAL EnableDelayedExpansion
 
 if "clean"=="%1" (
-    if exist bin\ rmdir /q /s bin
-) else if "cl"=="%1" (
+    if exist bin\ RMDIR /q /s bin
 
+) else if "cl"=="%1" (
     if "%VisualStudioVersion%"=="" call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-    if not exist bin\ mkdir bin
-    pushd bin
+    if not exist bin\ MKDIR bin
+    PUSHD bin
         cl /O2 /source-charset:utf-8 /Zi -I..\Include                   ^
             /DTARGETENV_X64_WINDOWS                                     ^
             ..\x64_Windows_Build.c /FeGraph.exe gdi32.lib user32.lib    ^
             /link /subsystem:console /stack:0x1000000
-    popd
+    POPD
+
 ) else (
-    if not exist bin\ mkdir bin
-    gcc -O0 -march=native -ggdb -Wall -Wextra -Wpedantic -IInclude  ^
+    if not exist bin\ MKDIR bin
+
+    SET COMPILE_OPT= ^
+        -O0 -march=native -ggdb -Wall -Wextra -Wpedantic -IInclude  ^
         -DTARGETENV_X64_WINDOWS                                     ^
         x64_Windows_Build.c -o bin\Graph.exe                        ^
-        -lgdi32 -luser32 -lkernel32 -Wl,--stack,0x1000000
+        -lgdi32 -luser32 -lkernel32 -Wl,--stack,0x1000000"
+
+    REM no compiler specified, use gcc as default
+    if "%1"=="" (
+        gcc !COMPILE_OPT!
+    ) else (
+        "%1" !COMPILE_OPT!
+    )
 )
